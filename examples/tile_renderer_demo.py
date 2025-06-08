9db8c8f44980ceb79d3c89eaab3bab874930318e
bisu <michalik.gregory@gmail.com>
2025-06-08 22:54:09 +0200

    Add movable player to tile renderer demo

--- examples/tile_renderer_demo.py
+++ examples/tile_renderer_demo.py
@@ -8,6 +8,9 @@ TILE_HEIGHT = 32
 MAP_WIDTH = 10
 MAP_HEIGHT = 10
 
+# Simple player sprite size
+PLAYER_RADIUS = 12
+
 # Colors for a few tile types
 TILE_COLORS = [
     (110, 170, 70),  # grass
@@ -52,6 +55,24 @@ class TileRenderer:
                 self.screen.blit(self.tiles[tile], (sx, sy))
 
 
+class Player:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        size = PLAYER_RADIUS * 2
+        self.surface = pygame.Surface((size, size), pygame.SRCALPHA)
+        pygame.draw.circle(self.surface, (255, 50, 50), (PLAYER_RADIUS, PLAYER_RADIUS), PLAYER_RADIUS)
+
+    def move(self, dx, dy):
+        self.x = max(0, min(MAP_WIDTH - 1, self.x + dx))
+        self.y = max(0, min(MAP_HEIGHT - 1, self.y + dy))
+
+    def draw(self, screen, origin):
+        sx = (self.x - self.y) * (TILE_WIDTH // 2) + origin[0]
+        sy = (self.x + self.y) * (TILE_HEIGHT // 2) + origin[1] - PLAYER_RADIUS
+        screen.blit(self.surface, (sx, sy))
+
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
@@ -59,16 +80,28 @@ def main():
 
     tiles = create_tile_surfaces()
     grid = random_map()
-    renderer = TileRenderer(screen, tiles, origin=(400, 100))
+    origin = (400, 100)
+    renderer = TileRenderer(screen, tiles, origin=origin)
+    player = Player(MAP_WIDTH // 2, MAP_HEIGHT // 2)
 
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    player.move(0, -1)
+                elif event.key == pygame.K_DOWN:
+                    player.move(0, 1)
+                elif event.key == pygame.K_LEFT:
+                    player.move(-1, 0)
+                elif event.key == pygame.K_RIGHT:
+                    player.move(1, 0)
 
         screen.fill((0, 0, 0))
         renderer.draw_map(grid)
+        player.draw(screen, origin)
         pygame.display.flip()
         clock.tick(60)
 
